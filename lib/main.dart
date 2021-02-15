a6c407fc62d80cc6261162524d9e2b430878f88a
Tom Curran <tom@tomcurran.org>
2021-02-15 21:18:43 +0000

    Added some loading / auth before app loads

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:strava_flutter/strava.dart';
 import './home-page.dart';
+import './secret.dart';
 
 void main() {
   runApp(StrutterApp());
 }
 
-class StrutterApp extends StatelessWidget {
+class StrutterApp extends StatefulWidget {
+  @override
+  _StrutterAppState createState() => _StrutterAppState();
+}
+
+class _StrutterAppState extends State<StrutterApp> {
+  final _strava = Strava(true, secret);
+  bool _isLoading = true;
+
+  void _load() async {
+    await _strava.oauth(clientId, 'activity:read_all', secret, 'auto');
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _load();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Strutter',
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      home: _isLoading
+          ? Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
+          : HomePage(),
     );
   }
 }
